12f97cd88560fa62cf1a77cb32d361f290001fb4
Евгений <brearey4@gmail.com>
2024-07-06 17:24:49 +0900

    I have problems with error call

--- src/app.js
+++ src/app.js
@@ -5,10 +5,10 @@ app.use(express.json());
 
 app.get('/api', (req, res) => {
     const userAge = Number(req.query.age)
-    if (isNaN(userAge)) {
-        process.exit(1) //This is an operational error: Invalid user input
-    }
-    res.status(200).send({message: `This is API v.1, your double age is ${userAge * 2}`})
+    // if (isNaN(userAge)) {
+    //     process.exit(1) //This is an operational error: Invalid user input
+    // }
+    res.status(200).send({message: `This is API v.1, your double age is ${userAge * req.query.usernumber}`})
 });
 
 app.get('/', (req, res) => {
