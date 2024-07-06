a16d2bf6723c0e6ef9e4845c1d83e4d9124fa0a8
Евгений <brearey4@gmail.com>
2024-07-06 16:52:57 +0900

    Added prod/dev into .env

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+require('dotenv').config()
 const app = require('./src/app')
 const PORT = process.env.PORT || 80;
 
-app.listen(PORT, () => console.log(`SERVER STARTED ON PORT: ${PORT}`))
+app.listen(PORT, () => console.log(`SERVER STARTED ON MODE ${app.get('env')} AND ON THE PORT: ${PORT}`))
 
 // да ладно это я зашел через браузер Chrome на слабом ноуте Aquarius через vscode.dev
