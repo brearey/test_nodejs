986c3fb1d2422d23a2774cfeed0c87ea14e78524
lorriant <brearey4@gmail.com>
2026-04-02 13:22:10 +0900

    buttons on click

--- src/esmodules/app.js
+++ src/esmodules/app.js
@@ -1,5 +1,25 @@
-import { log } from "./logger.mjs"
+import { log } from "./logger.js"
 
-console.log("begin app.js")
+const buttons = []
 
-log("My message")
+const container = document.getElementsByClassName("container").item(0)
+const btn1 = document.getElementById("btn1")
+const btn2 = document.getElementById("btn2")
+
+const btn1Click = () => {
+  buttons.push(btn1)
+  btn1.remove()
+  log(buttons)
+}
+
+const btn2Click = () => {
+  if (buttons.length > 0) {
+    container.appendChild(buttons[buttons.length - 1])
+    buttons.pop()
+    log(buttons)
+  }
+}
+
+btn1.addEventListener("click", btn1Click)
+
+btn2.addEventListener("click", btn2Click)
